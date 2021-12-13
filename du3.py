aeb88a2335c816c46b65e511b2c8638ac62dd527
Lukáš Nekola <nekola.luky@seznam.cz>
2021-12-13 11:14:20 +0100

    after first repair

--- du3.py
+++ du3.py
@@ -11,40 +11,40 @@ def loadGeoJson(inputJSON):
     except FileNotFoundError:
         print(f"Soubor {inputJSON} nebyl nalezen!!")
     except RuntimeError:
-        pass
+        print(f"Vyskytla se neočekávaná chyba v průběhu načítání souboru {inputJSON}. Zkontrolujte svůj software!!!")
     except ValueError:
         print(f"Soubor {inputJSON} obsahuje chybné hodnoty")
     except PermissionError:
         print(f"Přístup k souboru {inputJSON} byl zamítnut")
 
 #VÝBĚR A ÚPRAVA ATRIBUTŮ V SOUBORU S ADRESAMI
-def getDataAdress(inputAdress):
+def getDataAdress(inputAdress,wgsCo):
     adress = {} #vše bude uloženo ve slovníku
     for feature in inputAdress:
         street = feature["properties"]["addr:street"]
         houseNumber = feature["properties"]["addr:housenumber"]
         fullAdress = street + " " + houseNumber #celá adresa do jednoho řetězce
-        wgs = feature["geometry"]["coordinates"]
-        jtsk = wgs2jtsk.transform(wgs[0],wgs[1]) #transformace dat do jtsk
-        adress[fullAdress] = jtsk #přiřazení souřadnic k příslušné adrese
+        wgsCO = feature["geometry"]["coordinates"]
+        jtskCO = wgs2jtsk.transform(wgsCO[0],wgsCO[1]) #transformace dat do jtsk
+        adress[fullAdress] = jtskCO #přiřazení souřadnic k příslušné adrese
     return adress
 
 #VÝBĚR A ÚPRAVA ATRIBUTŮ V SOUBORU S KONTEJNERY
-def getDataConteiners(inputConteners):
-    conteiners = {}
-    for feature in inputConteners:
+def getDataContainers(inputContainers):
+    containers = {}
+    for feature in inputContainers:
         fullAdress = feature["properties"]["STATIONNAME"]
-        wgs = feature["geometry"]["coordinates"]
+        wgsCO = feature["geometry"]["coordinates"]
         if feature["properties"]["PRISTUP"] == "volně": # výběr pouze těch kontejnerů, které mají volný přístup
-            conteiners[fullAdress] = wgs
-    return conteiners
+            containers[fullAdress] = wgsCO
+    return containers
 
 #VÝPOČET VZDÁLENOSTI OD ADRES K NEJBLIŽŠÍMU VEŘEJNÉMU KONTEJNERU
-def distance(adress, conteiners):
+def distance(adress, containers):
     distances = {}
     for (adressesAd, coordinatesAd) in adress.items():
         onGoing = 10000 #proměnná s, která bude přepisována dle 
-        for (_,coordinatesCo) in conteiners.items():
+        for coordinatesCo in containers.values():
             finalDistance = sqrt(((coordinatesAd[0] - coordinatesCo[0])**2) + ((coordinatesAd[1] - coordinatesCo[1])**2)) #výpočet vzdálenosti na základě Pythagovovi věty
             if (finalDistance < onGoing): #neustále se zjišťuje vzdálenost k nejbližšímu kontejneru a přepíše se v proměnné onGoing
                 onGoing = finalDistance
@@ -56,12 +56,12 @@ def distance(adress, conteiners):
     return distances
 
 #VÝBĚR NEJVZDÁLENĚJŠÍHO KONTEJNERU
-def maxDistance(distances):
-    maximDistance = max(distances.values()) #získání největší vzdálenosti nejbližšího kontejneru z výběru adres
+def maxDistance(distances,maximDistance):
+     #získání největší vzdálenosti nejbližšího kontejneru z výběru adres
     for (adress, dis) in distances.items(): #cyklus, který zjistí na základě rovnosti adresu dle proměnné maximDistance
         if dis == maximDistance:
             maxstreet = adress
-    return maxstreet, maximDistance
+    return maxstreet
 
 #MEDIÁN
 def median(distances):
@@ -82,27 +82,28 @@ def median(distances):
 
 #SAMOTNÉ VYUŽITÍ FUNKCÍ
 #názvy vstupních souborů
-conteiners = "kontejnery.geojson"
+containers = "kontejnery.geojson"
 adress = "adresy.geojson"
 
 #načtení vstupních souborů
-conteiners = loadGeoJson(conteiners)
+containers = loadGeoJson(containers)
 adress = loadGeoJson(adress)
 
 wgs2jtsk = Transformer.from_crs(CRS.from_epsg(4326), CRS.from_epsg(5514), always_xy=True) #vytvoření transformeru pro převod
 
 #výběr a následná úprava vstupních dat
-generalizeAdress = getDataAdress(adress) 
-generalizeConteiners = getDataConteiners(conteiners)
+generalizeAdress = getDataAdress(adress,wgs2jtsk) 
+generalizeContainers = getDataContainers(containers)
 
 
-takeDistances = distance(generalizeAdress,generalizeConteiners) #výpočet vzdáleností na základě výše popsané vunkce
+takeDistances = distance(generalizeAdress,generalizeContainers) #výpočet vzdáleností na základě výše popsané vunkce
+maximDistance = max(takeDistances.values())
 averageDistance = sum(takeDistances.values()) / len(takeDistances) #průměr všech nejbližších vzáleností
-maxstreet = maxDistance(takeDistances)
+maxstreet = maxDistance(takeDistances,maximDistance)
 
 #tisk všech výstupů
 print(f"Nacteno {len(generalizeAdress)} adresnich bodu.")
-print(f"Nacteno {len(generalizeConteiners)} kontejneru na trideny odpad.\n")
+print(f"Nacteno {len(generalizeContainers)} kontejneru na trideny odpad.\n")
 print(f"Prumerna vzdalenost ke kontejneru je {median(takeDistances):.0f} m.") #vstup do mediánu z prověné takeDistances
 print(f"Medián vzdálenosti ke kontejneru je {averageDistance:.0f} m.")
-print(f"Nejdale ke kontejneru je z adresy {maxstreet[0]} a to {maxstreet[1]:.0f} m.\n")
+print(f"Nejdale ke kontejneru je z adresy {maxstreet} a to {maximDistance:.0f} m.\n")
