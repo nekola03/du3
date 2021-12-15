06780006a96c586fbaca69da4825c9450fb96ece
Lukáš Nekola <nekola.luky@seznam.cz>
2021-12-15 11:09:09 +0100

    the second attemp - repair list and arguments

--- du3.py
+++ du3.py
@@ -5,15 +5,15 @@ import argparse
 
 #názvy vstupních souborů
 containers = "kontejnery.geojson"
-adress = "adrsy.geojson"
+adress = "adresy.geojson"
 
 #PŘEDÁNÍ PARAMETRŮ
-parameters = argparse.ArgumentParser(description='Výpočet vzdálenosti ke kontejneru od jednotlivých adres')
-parameters.add_argument('-a', '--adresy', type = str, required = False, help = 'soubor s adresními body ve formátu GEOJSON')
-parameters.add_argument('-k', '--kontejnery', type = str, required = False, help = 'soubor s kontejnery ve formátu GEOJSON')
+parameters = argparse.ArgumentParser(description='Výpočet vzdálenosti k nejbližšímu kontejneru od jednotlivých adres')
+parameters.add_argument('-a', '--adresy', type = str, required = False, help = 'Soubor s adresními body ve formátu GEOJSON. Soubor musí obsahovat atribut adresy, domovního čísla společně s lokalizací v souřadnicovém systému WGS84.')
+parameters.add_argument('-k', '--kontejnery', type = str, required = False, help = 'Soubor s kontejnery ve formátu GEOJSON. Soubor musí obsahovat atribut adresy s domovním číslem společně s lokalizací v souřadnicovém systému JTSK.')
 args = parameters.parse_args()
 if args.adresy is not None: #dvojitý zápor, takže pokud existuje zadaný parametr
-    adress = args.adresy #zapíše se do proměné adresy
+    adress = args.adresy 
 elif args.kontejnery is not None:
     containers = args.kontejnery
 
@@ -25,15 +25,13 @@ def loadGeoJson(inputJSON):
             return file
     except FileNotFoundError:
         print(f"Soubor {inputJSON} nebyl nalezen!!")
-    except RuntimeError:
-        print(f"Vyskytla se neočekávaná chyba v průběhu načítání souboru {inputJSON}. Zkontrolujte svůj software!!!")
     except ValueError:
         print(f"Soubor {inputJSON} obsahuje chybné hodnoty")
     except PermissionError:
         print(f"Přístup k souboru {inputJSON} byl zamítnut")
 
 #VÝBĚR A ÚPRAVA ATRIBUTŮ V SOUBORU S ADRESAMI
-def getDataAdress(inputAdress,wgsCo):
+def getDataAdress(inputAdress,wgs2jtsk):
     adress = {} #vše bude uloženo ve slovníku
     for feature in inputAdress:
         street = feature["properties"]["addr:street"]
@@ -55,23 +53,23 @@ def getDataContainers(inputContainers):
     return containers
 
 #VÝPOČET VZDÁLENOSTI OD ADRES K NEJBLIŽŠÍMU VEŘEJNÉMU KONTEJNERU
-def distance(adress, containers, permissibleDis):
+def distance(adress, containers, PERMISSIBLE_DIS):
     distances = {}
     for (adressesAd, coordinatesAd) in adress.items():
         for coordinatesCo in containers.values():
             finalDistance = sqrt(((coordinatesAd[0] - coordinatesCo[0])**2) + ((coordinatesAd[1] - coordinatesCo[1])**2)) #výpočet vzdálenosti na základě Pythagovovi věty
-            if (finalDistance < permissibleDis): #neustále se zjišťuje vzdálenost k nejbližšímu kontejneru a přepíše se v proměnné permissibleDis
-                permissibleDis = finalDistance
-        if (permissibleDis >= 10000): #v případě vzdálenosti ke kontejneru větší než 10 km, program skončí
+            if (finalDistance < PERMISSIBLE_DIS): #neustále se zjišťuje vzdálenost k nejbližšímu kontejneru a přepíše se v proměnné PERMISSIBLE_DIS
+                PERMISSIBLE_DIS = finalDistance
+        if (PERMISSIBLE_DIS >= 10000): #v případě vzdálenosti ke kontejneru větší než 10 km, program skončí
             print(f"Vzdálenost veřejného kontejneru od adresy {adressesAd} je větší než 10 km.")
             print("!!!Program končí!!!")   
             exit()
-        distances[adressesAd] = permissibleDis #každá asresa se spojí vždy s nejbližší vzdáleností k veřejnému kontejneru
+        distances[adressesAd] = PERMISSIBLE_DIS #každá asresa se spojí vždy s nejbližší vzdáleností k veřejnému kontejneru
     return distances
 
 #VÝBĚR NEJVZDÁLENĚJŠÍHO KONTEJNERU
 def maxDistance(distances):
-    maximDistance = max(takeDistances.values()) #získání největší vzdálenosti nejbližšího kontejneru z výběru adres
+    maximDistance = max(distances.values()) #získání největší vzdálenosti nejbližšího kontejneru z výběru adres
     for (adress, dis) in distances.items(): #cyklus, který zjistí na základě rovnosti adresu dle proměnné maximDistance
         if dis == maximDistance:
             maxstreet = adress
@@ -79,23 +77,26 @@ def maxDistance(distances):
 
 #MEDIÁN
 def median(distances):
+    """
     distancesList = [] 
     for (_,dis) in distances.items(): #pro snazší práci se vybere ze slovníku pouze vzdálenost a zapíše do pole
         distancesList.append(dis)
     distancesList.sort() #sezaření vzestupně
+    """
+    distancesList = sorted(list(distances.values()))
     if ((len(distancesList) % 2) == 0): #výpočet pozic okolních hodnot mediánu v případě sudého počtu hodnot 
-        medPositionLower = int((len(distancesList)) / 2)
-        medPositionHigher = int((len(distancesList) + 2) / 2)
+        medPositionLower = (len(distancesList)) // 2
+        medPositionHigher = (len(distancesList) + 2) // 2
     elif ((len(distancesList) % 2) == 1): #výpočet pozic okolních hodnot mediánu v případě lichého počtu hodnot 
-        medPositionLower = int((len(distancesList) - 1) / 2)
-        medPositionHigher = int((len(distancesList) + 1) / 2)
+        medPositionLower = (len(distancesList) - 1) // 2
+        medPositionHigher = (len(distancesList) + 1) // 2
     medValueLower = distancesList[medPositionLower] #zjištění hodnoty na základě pozicev listu
     medValueHigher =  distancesList[medPositionHigher]
     medValue = (medValueLower + medValueHigher) / 2 #výpočet průměru z okolních hodnot
     return medValue
 
 #SAMOTNÉ VYUŽITÍ FUNKCÍ
-PERMISSIBLEDIS = 10000 #maximální vzdálenost kontejnerů braných v úvahu
+PERMISSIBLE_DIS = 10000 #maximální vzdálenost kontejnerů braných v úvahu
 
 #načtení vstupních souborů
 containers = loadGeoJson(containers)
@@ -108,7 +109,7 @@ generalizeAdress = getDataAdress(adress,wgs2jtsk)
 generalizeContainers = getDataContainers(containers)
 
 
-takeDistances = distance(generalizeAdress,generalizeContainers, PERMISSIBLEDIS) #výpočet vzdáleností na základě výše popsané vunkce
+takeDistances = distance(generalizeAdress,generalizeContainers, PERMISSIBLE_DIS) #výpočet vzdáleností na základě výše popsané vunkce
 averageDistance = sum(takeDistances.values()) / len(takeDistances) #průměr všech nejbližších vzáleností
 maxstreet = maxDistance(takeDistances)
 
